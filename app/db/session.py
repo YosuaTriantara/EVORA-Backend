ce681d493ffd3eda7ec375f5236f3fd119e9fa12
YosuaTriantara <yosuatriantara@gmail.com>
2026-05-04 18:03:50 +0800

    add: requirements.txt

--- app/db/session.py
+++ app/db/session.py
@@ -6,20 +6,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Pastikan di .env DATABASE_URL menggunakan format: 
-# postgresql://postgres:[password]@db.[ref].supabase.co:5432/postgres
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Engine: Jembatan ke Supabase
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    # Pool size disesuaikan agar tidak melebihi kuota koneksi Supabase Free Tier
     pool_size=5, 
     max_overflow=10
 )
 
-# SessionLocal: Pabrik untuk membuat sesi database per request API
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base: Class induk untuk semua Models kita
 Base = declarative_base()
